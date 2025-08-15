a86935f59b65f926f6945039ab345dd3c909f1f0
Alexandra Seligová <alexaseligova@gmail.com>
2025-08-15 06:54:39 +0200

    [Add] Base pages for demo: ContainersForProcessing, InboundContainers, OutboundContainers
    - Added ContainersForProcessingView (XAML + code-behind) with two DataGrids for operations and current contents
    - Added InboundContainersView with header info panel and two DataGrids
    - Added OutboundContainersView with header info panel and single DataGrid
    - Included test data in code-behind for initial demo presentation
    - Updated project file to include new pages

--- SVN_tOrder/tOrder/tOrder_UI/tOrder/UI/Pages/CapacityUnitDashboard/InfoContent/TabContent/ContainersForProcessingView.xaml.cs
+++ SVN_tOrder/tOrder/tOrder_UI/tOrder/UI/Pages/CapacityUnitDashboard/InfoContent/TabContent/ContainersForProcessingView.xaml.cs
@@ -1,44 +1,75 @@
-﻿//===================================================================
-// $Workfile:: ContainersForProcessingView.xaml.cs                   $
-// $Author:: Alexandra_Seligova                                     $
-// $Revision:: 1                                                    $
-// $Date:: 2025-08-15 23:42:00 +0200 (čt, 15 srp 2025)              $
-//===================================================================
-// Description: SPC - tOrder
-//     View for tab "Containers for Processing" in InfoContentC.
-//     Displays order detail fields and flags for further processing.
-//===================================================================
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.ObjectModel;
 
-namespace tOrder.UI;
-
-//-----------------------------------------------------------
-#region Using directives
-//-----------------------------------------------------------
-using Microsoft.UI.Xaml.Controls;
-using tOrder.Common;
-
-#endregion // Using directives
-
-//===================================================================
-// class ContainersForProcessingView
-//===================================================================
-
-public sealed partial class ContainersForProcessingView : UserControl
+namespace tOrder.UI
 {
-
-
-    public LayoutConfigVM LayoutConfig => App.GetService<LayoutConfigVM>();
-
-    //-----------------------------------------------------------
-    #region Constructor
-    //-----------------------------------------------------------
-
-    public ContainersForProcessingView()
+    public sealed partial class ContainersForProcessingView : UserControl
     {
-        this.InitializeComponent();
-        this.DataContext = this;
+        public LayoutConfigVM LayoutConfig => App.GetService<LayoutConfigVM>();
+
+        public ObservableCollection<OperationItem> Operations { get; } = new();
+        public ObservableCollection<ContentItem> CurrentContents { get; } = new();
+
+        public ContainersForProcessingView()
+        {
+            this.InitializeComponent();
+
+            // Sample data for first table
+            Operations.Add(new OperationItem
+            {
+                Datum = "20.5.2024 13:13:13",
+                BehNr = "111",
+                RueckmNr = "999999",
+                Schicht = "Tagschicht",
+                Bediener = "1712",
+                ProdAktivitaet = "Mikron Multistar mit NX",
+                KapStelle = "MST-03",
+                Tz = "10",
+                ArbPlan = "F 00V H28 042/2",
+                GesamtStk = "6666"
+            });
+
+            // Sample data for second table
+            CurrentContents.Add(new ContentItem
+            {
+                Datum = "20.5.2024 21:21:21",
+                Schicht = "Nachtschicht 1",
+                Bediener = "1797 Wilhelm I.",
+                Zuwachs = "+2222 Stk.",
+                Gesamtmenge = "2222 Stk."
+            });
+            CurrentContents.Add(new ContentItem
+            {
+                Datum = "20.5.2024 23:23:23",
+                Schicht = "Nachtschicht 2",
+                Bediener = "1831 Friedrich III.",
+                Zuwachs = "+4444 Stk.",
+                Gesamtmenge = "6666 Stk."
+            });
+        }
     }
 
-    #endregion // Constructor
+    public class OperationItem
+    {
+        public string Datum { get; set; }
+        public string BehNr { get; set; }
+        public string RueckmNr { get; set; }
+        public string Schicht { get; set; }
+        public string Bediener { get; set; }
+        public string ProdAktivitaet { get; set; }
+        public string KapStelle { get; set; }
+        public string Tz { get; set; }
+        public string ArbPlan { get; set; }
+        public string GesamtStk { get; set; }
+    }
+
+    public class ContentItem
+    {
+        public string Datum { get; set; }
+        public string Schicht { get; set; }
+        public string Bediener { get; set; }
+        public string Zuwachs { get; set; }
+        public string Gesamtmenge { get; set; }
+    }
 }
-//===================================================================
--- SVN_tOrder/tOrder/tOrder_UI/tOrder/UI/Pages/CapacityUnitDashboard/InfoContent/TabContent/OutboundContainersView.xaml.cs
+++ SVN_tOrder/tOrder/tOrder_UI/tOrder/UI/Pages/CapacityUnitDashboard/InfoContent/TabContent/OutboundContainersView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.UI.Xaml.Controls;
+using System.Collections.ObjectModel;
 
 namespace tOrder.UI
 {
@@ -6,9 +7,35 @@ namespace tOrder.UI
     {
         public LayoutConfigVM LayoutConfig => App.GetService<LayoutConfigVM>();
 
+        public ObservableCollection<OutboundContent> CurrentContents { get; set; }
+
         public OutboundContainersView()
         {
             this.InitializeComponent();
+
+            // Sample data
+            CurrentContents = new ObservableCollection<OutboundContent>
+            {
+                new OutboundContent
+                {
+                    Datum = "",
+                    Schicht = "",
+                    Bediener = "",
+                    Zuwachs = "",
+                    Gesamtmenge = ""
+                }
+            };
+
+            this.DataContext = this;
         }
+    }
+
+    public class OutboundContent
+    {
+        public string Datum { get; set; }
+        public string Schicht { get; set; }
+        public string Bediener { get; set; }
+        public string Zuwachs { get; set; }
+        public string Gesamtmenge { get; set; }
     }
 }
